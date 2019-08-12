ecb0310834455fc5b678a96c29a1b52df186b221
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-12 20:14:16 +0700

    first commit

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragment10052019/AndroidFragment.java
@@ -0,0 +1,26 @@
+package khoapham.ptp.phamtanphat.fragment10052019;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AndroidFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_android, container, false);
+        return view;
+    }
+
+}
