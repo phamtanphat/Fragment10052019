555bc09f43f2f1a70e23e332398047bf604378ed
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-12 20:52:02 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/fragment10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragment10052019/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAddFragment;
+    Button btnAddFragmentAndroid,btnAddFragmentPhp;
     FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +20,24 @@ public class MainActivity extends AppCompatActivity {
         fragmentManager = getSupportFragmentManager();
 
 
-        btnAddFragment = findViewById(R.id.buttonAddFragment);
-        btnAddFragment.setOnClickListener(new View.OnClickListener() {
+        btnAddFragmentAndroid = findViewById(R.id.buttonAddFragmentAndroid);
+        btnAddFragmentPhp = findViewById(R.id.buttonAddFragmentPhp);
+
+        btnAddFragmentAndroid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 AndroidFragment androidFragment = new AndroidFragment();
                 fragmentTransaction.add(R.id.frameContain, androidFragment);
+                fragmentTransaction.commit();
+            }
+        });
+        btnAddFragmentPhp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                PhpFragment phpFragment = new PhpFragment();
+                fragmentTransaction.add(R.id.frameContain, phpFragment);
                 fragmentTransaction.commit();
             }
         });
