fec1899e05770f5ae233b89f57e043fe864f9614
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-12 21:04:49 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/fragment10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragment10052019/MainActivity.java
@@ -1,6 +1,7 @@
 package khoapham.ptp.phamtanphat.fragment10052019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -9,37 +10,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAddFragmentAndroid,btnAddFragmentPhp;
-    FragmentManager fragmentManager;
+    FragmentManager fragmentManager = getSupportFragmentManager();;
+    Fragment fragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragmentManager = getSupportFragmentManager();
 
 
         btnAddFragmentAndroid = findViewById(R.id.buttonAddFragmentAndroid);
         btnAddFragmentPhp = findViewById(R.id.buttonAddFragmentPhp);
 
-        btnAddFragmentAndroid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                AndroidFragment androidFragment = new AndroidFragment();
-                fragmentTransaction.add(R.id.frameContain, androidFragment);
-                fragmentTransaction.commit();
-            }
-        });
-        btnAddFragmentPhp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                PhpFragment phpFragment = new PhpFragment();
-                fragmentTransaction.add(R.id.frameContain, phpFragment);
-                fragmentTransaction.commit();
-            }
-        });
+        btnAddFragmentPhp.setOnClickListener(MainActivity.this);
+        btnAddFragmentAndroid.setOnClickListener(MainActivity.this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        switch (view.getId()){
+            case R.id.buttonAddFragmentAndroid :
+                fragment = new AndroidFragment();
+                break;
+            case R.id.buttonAddFragmentPhp:
+                fragment = new PhpFragment();
+                break;
+        }
+        fragmentTransaction.add(R.id.frameContain,fragment);
+        fragmentTransaction.commit();
     }
 }
