22d8fd562807abf25a367ed1214e2c8508e58888
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-12 20:44:14 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/fragment10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragment10052019/MainActivity.java
@@ -1,14 +1,34 @@
 package khoapham.ptp.phamtanphat.fragment10052019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAddFragment;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+
+        btnAddFragment = findViewById(R.id.buttonAddFragment);
+        btnAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                AndroidFragment androidFragment = new AndroidFragment();
+                fragmentTransaction.add(R.id.frameContain, androidFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
